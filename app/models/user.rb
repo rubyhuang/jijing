9375ecaf5194e939a086b0d65955d9ca512eaffe
derk <derk.tu@gmail.com>
2012-11-20 23:36:42 +0800

    add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  attr_accessor :login
+  attr_accessible :login
 
   def name
     self.email.split('@').first
   end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
   
   # Get the updated posts number from last visited on the following profile page
   def following_posts_count
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726131455) do
+ActiveRecord::Schema.define(:version => 20121120150757) do
 
   create_table "follows", :force => true do |t|
     t.integer  "followable_id",                      :null => false
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(:version => 20120726131455) do
     t.string   "picture_file_size"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
